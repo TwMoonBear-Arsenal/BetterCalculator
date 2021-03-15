a0e62956975b4310c6aa891ed16df5c6a688185d
iYarYar <iYarYar@users.noreply.github.com>
2021-03-15 20:41:26 +0800

    Update betterCalculator.py

--- source/betterCalculator.py
+++ source/betterCalculator.py
@@ -11,7 +11,7 @@ class BetterCalculator:
     def Pow(x, y):
         for i in range(y-1):
             x = x * x
-        return x  # 庭維
+        return x  # 庭維上傳囉~~~
 
     @staticmethod
     def Gcd(x, y):

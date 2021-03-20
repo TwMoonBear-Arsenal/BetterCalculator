d0f0334f05f83cfb855106d0c4d097227284a71d
Carlton Chen <carlton0521@gmail.com>
2021-03-20 14:14:38 +0800

    取消公約數，新增四則運算測試

--- source/test.py
+++ source/test.py
@@ -1,22 +1,23 @@
 import pytest
 from betterCalculator import BetterCalculator
 
+def test_Add():
+    assert(BetterCalculator.Add(5, 3) == 8)
 
-def test_AlwaysTrue():
-    assert 1 == 1
+def test_Minus():
+    assert(BetterCalculator.Minus(5, 3) == 2)
 
+def test_Multiple():
+    assert(BetterCalculator.Multiple(5, 3) == 15)
 
+def test_Divide():
+    assert(BetterCalculator.Divide(15, 3) == 5)
+    
 def test_Mod():
     assert(BetterCalculator.Mod(5, 3) == 2)
 
-
 def test_Conj():
     assert(BetterCalculator.Conj(5, 3) == "53")
-
-
-def test_Gcd():
-    assert(BetterCalculator.Gcd(15, 9) == 3)
-
 
 def test_Pow():
     assert(BetterCalculator.Pow(5, 3) == 125)
